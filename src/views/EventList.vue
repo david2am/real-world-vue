1d2cc42e1ff9e6cf76333fe3affceb4123b090d1
David De Vargas <david.devargas@zemoga.com>
2022-07-13 16:09:43 -0500

    feat(EventList): event api call & iterate over the envent list

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,16 +1,27 @@
 <template>
   <div>
     <h1>Event listing</h1>
-    <EventCard />
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
 <script>
+import EventService from '@/services/EventService'
 import EventCard from '@/components/EventCard'
 
 export default {
   components: {
     EventCard,
+  },
+  data() {
+    return {
+      events: [],
+    }
+  },
+  created() {
+    EventService.getEvents()
+      .then((response) => (this.events = response.data))
+      .catch((error) => console.log({ error }))
   },
 }
 </script>
